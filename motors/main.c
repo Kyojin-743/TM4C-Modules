5e7383ef698d56f9237a07aac9e254d3058d2351
Kyojin-743 <andme2003@gmail.com>
2026-07-16 00:00:18 -0500

    testing arming sequence, no luck (checkd 50 & 400 HZ frequencies with pulses of 1000us - 2000us however still not arming

--- motors/main.c
+++ motors/main.c
@@ -23,38 +23,52 @@
 /* GLOBALS CONSTS AND MACROS */
 #define LED_G           PORTF,3
 
-#define M1A PORTB,6     //M0 PWM 0  0A
-#define M1B PORTB,4     //M0 PWM 2  1A
-#define M1C PORTB,5     //M0 PWM 3  1B
+#define M1              PORTC,4     //M0PWM6 : M0 PWM3 GENA
+#define M2              PORTC,5     //M0PWM7 : M0 PWM3 GENB
+#define M3              PORTD,0     //M1PWM0 : M0 PWM0 GENA
+#define M4              PORTD,1     //M1PWM1 : M0 PWM0 GENB
 
-#define AINP             PORTE,1
-#define BINP             PORTE,2
-#define CINP             PORTE,4
+#define JLX             PORTE,5
+#define JLY             PORTE,4
+#define JRX             PORTE,1
+#define JRY             PORTE,2
+
+#define PWM_DIV         SYSCTL_RCC_PWMDIV_2
+#define PWM_LOAD        50000-1     //20ms period (50Hz)
+#define PWM_MIN         19000       //cmp min for 950us
+#define PWM_MAX         41000       //cmp max for 2050u
+#define PWM_RANGE       ((PWM_MAX)-(PWM_MIN))
 
 /* SUB ROUTINE PROTOTYPES */
 void initHw(void);
 
 void initAdc(void);
 void readAdc0Ss0(int16_t* data);
-void convert(int16_t* raws, uint16_t* norms);
+void mix(int16_t* raws, uint16_t *mixed);
 
 void initBldc(void);
-void setPwms(uint16_t a, uint16_t b, uint16_t c);
+void setPwms(uint16_t *pwms);
 
 /* MAIN ROUTINE */
 int main(void)
 {
     initHw();
 
-    int16_t raws[3];
-    uint16_t pwms[3];
+    int16_t raws[4];
+    uint16_t pwms[4];
 
-
+    putsUart0(CLEAR_SCREEN);
     for(;;) {
         readAdc0Ss0(raws);
-        convert(raws, pwms);
-        setPwms(pwms[0], 0,0);
-        waitMicrosecond(100e3);
+        mix(raws, pwms);
+//        convert(pwms);
+        setPwms(pwms);
+        char buffer[50];
+        usprintf(buffer, "M1:%d   \nM2:%d   \nM3:%d   \nM4:%d   ", pwms[0], pwms[1], pwms[2], pwms[3]);
+        putsUart0(SAVE_POS);
+        putsUart0(buffer);
+        putsUart0(RETURN_2_POS);
+        waitMicrosecond(1e3);
     }
 }
 
@@ -66,6 +80,7 @@ void initHw(void) {
     selectPinPushPullOutput(LED_G);
 
     initBldc();
+    setPwms((uint16_t[4]){PWM_MIN, PWM_MIN, PWM_MIN, PWM_MIN});
     initAdc();
 
     //Shell
@@ -80,101 +95,156 @@ void initHw(void) {
     setPinValue(LED_G, 1);
     waitMicrosecond(500e3);
     setPinValue(LED_G, 0);
-
-
 }
 
 void initAdc(void) {
-    //AINP             PORTE,1
-    //BINP             PORTE,2
-    //CINP             PORTE,4
+    //Joystick Analogs
+        enablePort(PORTE);
+        SYSCTL_RCGCADC_R |= SYSCTL_RCGCADC_R0;
+        selectPinAnalogInput(JLX);
+        selectPinAnalogInput(JLY);
+        selectPinAnalogInput(JRX);
+        selectPinAnalogInput(JRY);
+        setPinAuxFunction(JLX, GPIO_PCTL_PE5_AIN8);
+        setPinAuxFunction(JLY, GPIO_PCTL_PE4_AIN9);
+        setPinAuxFunction(JRX, GPIO_PCTL_PE1_AIN2);
+        setPinAuxFunction(JRY, GPIO_PCTL_PE2_AIN1);
 
-    SYSCTL_RCGCADC_R |= SYSCTL_RCGCADC_R0;
-    enablePort(PORTE);
-
-    selectPinAnalogInput(AINP);
-    selectPinAnalogInput(BINP);
-    selectPinAnalogInput(CINP);
-    setPinAuxFunction(AINP, GPIO_PCTL_PE1_AIN2);
-    setPinAuxFunction(BINP, GPIO_PCTL_PE2_AIN1);
-    setPinAuxFunction(CINP, GPIO_PCTL_PE4_AIN9);
-
-
-    //Disable ss0 for programming
-    ADC0_ACTSS_R &= ~ ADC_ACTSS_ASEN0;
-    //Use PLL Clock
-    ADC0_CC_R = ADC_CC_CS_SYSPLL;
-    //1Msps sample rate
-    ADC0_PC_R = ADC_PC_SR_1M;
-    ADC0_EMUX_R = ADC_EMUX_EM0_PROCESSOR;
-    //Set HW sampling
-    ADC0_SAC_R = ADC_SAC_AVG_16X;
+        //Disable ss0 for programming
+        ADC0_ACTSS_R &= ~ ADC_ACTSS_ASEN0;
+        //Use PLL Clock
+        ADC0_CC_R = ADC_CC_CS_SYSPLL;
+        //1Msps sample rate
+        ADC0_PC_R = ADC_PC_SR_1M;
+        ADC0_EMUX_R = ADC_EMUX_EM0_PROCESSOR;
+        //Set HW sampling
+        ADC0_SAC_R = ADC_SAC_AVG_16X;
 #define ADC_CTL_DITHER 0x00000040
-    ADC0_CTL_R |= ADC_CTL_DITHER;
-    //Set the sample sequence to read all pins in a burst
-    ADC0_SSMUX0_R = (2 << ADC_SSMUX0_MUX0_S) |  // PE1 AIN2 => AINP
-                    (1 << ADC_SSMUX0_MUX1_S) |  // PE2 AIN1 => BINP
-                    (9 << ADC_SSMUX0_MUX3_S);   // PE4 AIN9 => CINP
-    //Mark fourth sample as the end (and trigger interrupt used for dma)
-    ADC0_SSCTL0_R = ADC_SSCTL0_END2 | ADC_SSCTL0_IE2;
-    ADC0_ACTSS_R |= ADC_ACTSS_ASEN0;
+        ADC0_CTL_R |= ADC_CTL_DITHER;
+#undef ADC_CTL_DITHER
+        //Set the sample sequence to read all pins in a burst
+        ADC0_SSMUX0_R = (2 << ADC_SSMUX0_MUX0_S) |  // PE1 AIN2 => JRX
+                        (1 << ADC_SSMUX0_MUX1_S) |  // PE2 AIN1 => JRY
+                        (9 << ADC_SSMUX0_MUX3_S) |  // PE4 AIN9 => JLX
+                        (8 << ADC_SSMUX0_MUX2_S);   // PE5 AIN8 => JLY
+        //Mark fourth sample as the end (and trigger interrupt used for dma)
+        ADC0_SSCTL0_R = ADC_SSCTL0_END3 | ADC_SSCTL0_IE3;
+        ADC0_ACTSS_R |= ADC_ACTSS_ASEN0;
 }
 
-void readAdc0Ss0(int16_t* data) {
+void readAdc0Ss0(int16_t* data)
+{
     ADC0_PSSI_R |= ADC_PSSI_SS0;                    // set start bit
+    uint8_t i = 0;
     while (ADC0_ACTSS_R & ADC_ACTSS_BUSY);          // wait until SS3 is not busy
     while (ADC0_SSFSTAT0_R & ADC_SSFSTAT0_EMPTY);
-    uint8_t i = 0;
-    for(;i < 3; ++i)
+    for(;i < 4; ++i) {
         data[i] = (int16_t)ADC0_SSFIFO0_R;          // get single result from the FIFO
-}
-
-//  1 << 16 / 4096 * 1024 = 16384
-#define min(a,b) ((a)>(b)?b:a)
-void convert(int16_t* raws, uint16_t* norms) {
-    norms[0] = min(1023,(4096 - raws[0]) * 1024 / 4096);
-    norms[1] = min(1023,(4096 - raws[1]) * 1024 / 4096);
-    norms[2] = min(1023,(4096 - raws[2]) * 1024 / 4096);
+    }
 }
 
 void initBldc() {
+    enablePort(PORTC);
+    enablePort(PORTD);
 
-    SYSCTL_RCGCPWM_R |= SYSCTL_RCGCPWM_R0;
-    enablePort(PORTB);
-    selectPinPushPullOutput(M1A);
-    setPinAuxFunction(M1A, GPIO_PCTL_PB6_M0PWM0);
-    setPinAuxFunction(M1B, GPIO_PCTL_PB4_M0PWM2);
-    setPinAuxFunction(M1C, GPIO_PCTL_PB5_M0PWM3);
+    //enable pins
+    selectPinPushPullOutput(M1);
+    selectPinPushPullOutput(M2);
+    selectPinPushPullOutput(M3);
+    selectPinPushPullOutput(M4);
 
-    // Configure PWM module 1 to drive RGB LED
-    //M0 PWM 0 -> pwm0A
-    //M0 PWM 2 -> PWM1A
-    //M0 PWM 3 -> PWM1B
-    SYSCTL_SRPWM_R = SYSCTL_SRPWM_R0;
+    //set to use pwm
+    setPinAuxFunction(M1, GPIO_PCTL_PC4_M0PWM6);
+    setPinAuxFunction(M2, GPIO_PCTL_PC5_M0PWM7);
+    setPinAuxFunction(M3, GPIO_PCTL_PD0_M1PWM0);
+    setPinAuxFunction(M4, GPIO_PCTL_PD1_M1PWM1);
+
+    //PWM (Motors)
+    //MOTOR_FRONT_LEFT  (M1)    PC4 M0PWM6 : M0 PWM3 GENA
+    //MOTOR_FRONT_RIGHT (M2)    PC5 M0PWM7 : M0 PWM3 GENB
+    //MOTOR_BACK_LEFT   (M3)    PD0 M1PWM0 : M0 PWM0 GENA
+    //MOTOR_BACK_RIGHT  (M4)    PD1 M1PWM1 : M0 PWM0 GENB
+
+    SYSCTL_RCGCPWM_R |= SYSCTL_RCGCPWM_R0 | SYSCTL_RCGCPWM_R1;
+    SYSCTL_RCC_R |= SYSCTL_RCC_USEPWMDIV | PWM_DIV;
+    _delay_cycles(3);
+
+    //Reset
+    SYSCTL_SRPWM_R = SYSCTL_SRPWM_R1 | SYSCTL_SRPWM_R0;
     SYSCTL_SRPWM_R = 0;
 
-    PWM0_0_CTL_R = 0;
-    PWM0_1_CTL_R = 0;
+    //Turn off
+    PWM0_3_CTL_R = 0;
+    PWM1_0_CTL_R = 0;
 
-    PWM0_0_GENA_R = PWM_0_GENA_ACTCMPAD_ONE | PWM_0_GENA_ACTLOAD_ZERO;
-    PWM0_1_GENA_R = PWM_1_GENA_ACTCMPAD_ONE | PWM_1_GENA_ACTLOAD_ZERO;
-    PWM0_1_GENB_R = PWM_1_GENB_ACTCMPBD_ONE | PWM_1_GENB_ACTLOAD_ZERO;
+    //Configure
+    PWM0_3_GENA_R = PWM_3_GENA_ACTCMPAD_ONE | PWM_3_GENA_ACTLOAD_ZERO;
+    PWM0_3_GENB_R = PWM_3_GENB_ACTCMPBD_ONE | PWM_3_GENB_ACTLOAD_ZERO;
 
-    PWM0_0_LOAD_R = 1024;
-    PWM0_1_LOAD_R = 1024;
+    PWM1_0_GENA_R = PWM_0_GENA_ACTCMPAD_ONE | PWM_0_GENA_ACTLOAD_ZERO;
+    PWM1_0_GENB_R = PWM_0_GENB_ACTCMPBD_ONE | PWM_0_GENB_ACTLOAD_ZERO;
 
-    PWM0_0_CMPA_R = 0;
-    PWM0_1_CMPB_R = 0;
-    PWM0_1_CMPA_R = 0;
+    PWM0_3_LOAD_R = PWM_LOAD;
+    PWM1_0_LOAD_R = PWM_LOAD;
 
-    PWM0_0_CTL_R = PWM_0_CTL_ENABLE;
-    PWM0_1_CTL_R = PWM_1_CTL_ENABLE;
-    PWM0_ENABLE_R = PWM_ENABLE_PWM0EN | PWM_ENABLE_PWM2EN | PWM_ENABLE_PWM3EN;
+    PWM0_3_CMPA_R = 0;
+    PWM0_3_CMPB_R = 0;
+    PWM1_0_CMPA_R = 0;
+    PWM1_0_CMPB_R = 0;
+
+    //Turn on Generators
+    PWM0_3_CTL_R = PWM_0_CTL_ENABLE;
+    PWM1_0_CTL_R = PWM_1_CTL_ENABLE;
+
+    PWM0_SYNC_R = PWM_SYNC_SYNC3;
+    PWM1_SYNC_R = PWM_SYNC_SYNC0;
+
+    //Enable Outputs
+    PWM0_ENABLE_R |= PWM_ENABLE_PWM6EN | PWM_ENABLE_PWM7EN;
+    PWM1_ENABLE_R |= PWM_ENABLE_PWM0EN | PWM_ENABLE_PWM1EN;
 }
 
-void setPwms(uint16_t a, uint16_t b, uint16_t c) {
-    PWM0_0_CMPA_R = a;
-    PWM0_1_CMPA_R = b;
-    PWM0_1_CMPB_R = c;
-}
+#define min(a,b) ((a)>(b)?(b):(a))
+#define max(a,b) ((a)<(b)?(b):(a))
+void mix(int16_t* raws, uint16_t *mixed){
+    //raws[0] -> RY -> Ritch
+    //raws[1] -> RX -> Roll
+    //raws[2] -> LY -> Throttle
+    //raws[3] -> Lx -> Yaw
+    uint8_t i;
+    int32_t norms[4];
 
+    enum {
+        THROTTLE=2,
+        YAW=3,
+        PITCH=0,
+        ROLL=1
+    };
+
+    for(i = 0; i < 4; ++i) { norms[i] = (4096 - raws[i]) * 100 / 4096; }
+    norms[PITCH] -= 50;
+    norms[ROLL] -= 50;
+    norms[YAW] -= 50;
+
+
+
+    //mix
+    mixed[0] = max(0, min(norms[THROTTLE] + norms[PITCH] + norms[ROLL] + norms[YAW], 100));
+    mixed[1] = max(0, min(norms[THROTTLE] + norms[PITCH] - norms[ROLL] - norms[YAW], 100));
+    mixed[2] = max(0, min(norms[THROTTLE] - norms[PITCH] + norms[ROLL] - norms[YAW], 100));
+    mixed[3] = max(0, min(norms[THROTTLE] - norms[PITCH] - norms[ROLL] + norms[YAW], 100));
+
+    for(i = 0; i < 4; ++i)
+        mixed[i] = PWM_MIN + ((int32_t) mixed[i] * PWM_RANGE / 100);
+
+}
+#undef min
+#undef max
+
+
+void setPwms(uint16_t *pwms) {
+    PWM0_3_CMPA_R = pwms[0];
+    PWM0_3_CMPB_R = pwms[1];
+    PWM1_0_CMPA_R = pwms[2];
+    PWM1_0_CMPB_R = pwms[3];
+}
